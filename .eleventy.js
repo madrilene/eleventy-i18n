3479b98b0384019355cccac325333ef2751c502c
madrilene <hola@lenesaile.com>
2023-11-10 13:16:14 +0100

    adding x-default hreflang

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ const {EleventyI18nPlugin} = require('@11ty/eleventy');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
-    defaultLanguage: 'en' // Required
+    defaultLanguage: 'en', // Required
+    errorMode: 'allow-fallback' // Opting out of "strict"
   });
 
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
